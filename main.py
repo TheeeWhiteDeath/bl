bd7f9a39a5972d9c25d8a991b5997a9e3863f047
TheeeWhiteDeath <144166856+TheeeWhiteDeath@users.noreply.github.com>
2024-05-13 23:12:05 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 from selenium import webdriver
-import time
+
 def open_website_with_proxy(url, proxy):
     # Налаштування драйвера Chrome для використання headless режиму
     chrome_options = webdriver.ChromeOptions()
-
+    chrome_options.add_argument('--headless')  # Включення headless режиму
+    chrome_options.add_argument('--no-sandbox')  # Додаткові налаштування для безпечного запуску у віртуальному середовищі
     chrome_options.add_argument('executable_path=./chromedriver.exe')  # Вказуємо шлях до виконуваного файлу драйвера
 
     # Завантаження драйвера Chrome без запуску браузера
@@ -13,12 +14,9 @@ def open_website_with_proxy(url, proxy):
     driver.get(url)
     
     # Закриваємо драйвер
-    time.sleep(10)
     driver.quit()
-    
 
 url = 'https://www.example.com'
 proxy = 'ваш_проксі'
-
 
 open_website_with_proxy(url, proxy)
