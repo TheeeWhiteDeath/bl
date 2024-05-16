c6753670ce343e872875bf16e7867075ce170e79
TheeeWhiteDeath <144166856+TheeeWhiteDeath@users.noreply.github.com>
2024-05-16 21:33:10 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,16 @@
 from selenium import webdriver
 
-# Создаем новый экземпляр браузера Chrome
-driver = webdriver.Chrome()
+# Путь к geckodriver
+geckodriver_path = "/data/data/com.termux/files/usr/bin/geckodriver"
 
-# Открываем веб-сайт
-driver.get('https://www.google.com')
+# Путь к исполняемому файлу Firefox
+firefox_binary_path = "/data/data/com.termux/files/home/firefox/firefox-bin"
 
-# Закрываем браузер после использования
+# Создание экземпляра драйвера
+driver = webdriver.Firefox(executable_path=geckodriver_path, firefox_binary=firefox_binary_path)
+
+# Пример использования: открываем сайт Google
+driver.get("https://www.google.com")
+
+# Закрываем драйвер после использования
 driver.quit()
