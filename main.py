434eaecfb1db3a7d70e3188bffe23f2a10b63a2d
TheeeWhiteDeath <144166856+TheeeWhiteDeath@users.noreply.github.com>
2024-05-16 21:03:43 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,10 @@
 from selenium import webdriver
 
-# Путь к драйверу Chrome WebDriver
-chrome_driver_path = "./chromedriver"
+# Создаем новый экземпляр браузера Chrome
+driver = webdriver.Chrome()
 
-# Создание экземпляра драйвера Chrome с помощью Chrome WebDriver
-driver = webdriver.Chrome(executable_path=chrome_driver_path)
+# Открываем веб-сайт
+driver.get('https://www.google.com')
 
-# URL веб-страницы, которую вы хотите открыть
-url = "https://www.example.com"
-
-# Загрузка страницы
-driver.get(url)
+# Закрываем браузер после использования
+driver.quit()
