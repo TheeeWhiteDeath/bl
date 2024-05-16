beb0bd840a45b42f3003c53091fd8e2055314bf8
TheeeWhiteDeath <144166856+TheeeWhiteDeath@users.noreply.github.com>
2024-05-16 21:39:40 +0300

    Update main.py

--- main.py
+++ main.py
@@ -3,11 +3,8 @@ from selenium import webdriver
 # Путь к geckodriver
 geckodriver_path = "/data/data/com.termux/files/usr/bin/geckodriver"
 
-# Путь к исполняемому файлу Firefox
-firefox_binary_path = "/data/data/com.termux/files/home/firefox/firefox-bin"
-
 # Создание экземпляра драйвера
-driver = webdriver.Firefox(executable_path=geckodriver_path, firefox_binary=firefox_binary_path)
+driver = webdriver.Firefox(executable_path=geckodriver_path)
 
 # Пример использования: открываем сайт Google
 driver.get("https://www.google.com")
